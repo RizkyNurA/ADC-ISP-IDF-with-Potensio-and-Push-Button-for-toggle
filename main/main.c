79ff034e3f2fe104d6dafd1480ac558bec52f7ac
Abil <abilrizky04@gmail.com>
2026-03-27 16:33:58 +0700

    Menambahkan toggle dan memodularkan app_main

--- main/main.c
+++ main/main.c
@@ -3,18 +3,31 @@
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
 #include "driver/gpio.h"
-#include "FreeRTOS/freertos.h"
+#include "freertos/FreeRTOS.h"
 #include "esp_log.h"
+
+#define Pin_Button 22
 
 static int adc_raw[2][10];
 static int voltage[2][10];
 static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
 static void example_adc_calibration_deinit(adc_cali_handle_t handle);
 
-void app_main(void)
-{
+adc_oneshot_unit_handle_t adc1_handle;
+adc_cali_handle_t adc1_cali_chan0_handle;
+adc_cali_handle_t adc1_cali_chan2_handle;
+bool do_calibration1_chan0;
+bool do_calibration1_chan2;
+
+void GPIO_Initialation(){
+
+    gpio_reset_pin(Pin_Button);
+    gpio_set_direction(Pin_Button, GPIO_MODE_INPUT);
+}
+
+void ADC_Initialation(){
     //-------------ADC1 Init---------------//
-    adc_oneshot_unit_handle_t adc1_handle;
+    //adc_oneshot_unit_handle_t adc1_handle;
     adc_oneshot_unit_init_cfg_t init_config1 = {
         .unit_id = ADC_UNIT_1,
     };
@@ -29,20 +42,33 @@ void app_main(void)
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_2, &config));
 
     //-------------ADC1 Calibration Init---------------//
-    adc_cali_handle_t adc1_cali_chan0_handle = NULL;
-    adc_cali_handle_t adc1_cali_chan2_handle = NULL;
+    //adc_cali_handle_t adc1_cali_chan0_handle = NULL;
+    //adc_cali_handle_t adc1_cali_chan2_handle = NULL;
     bool do_calibration1_chan0 = example_adc_calibration_init(ADC_UNIT_1, ADC_CHANNEL_0, 3, &adc1_cali_chan0_handle);
     bool do_calibration1_chan2 = example_adc_calibration_init(ADC_UNIT_1, ADC_CHANNEL_2, 3, &adc1_cali_chan2_handle);
+}
 
-    while (1) {
-        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &adc_raw[0][0]));
+int button_pressed(int state){
+	static int last_state = 0;
+	int ret = 0;
+
+	// Detect rising edge(button just pressed)
+	if (!last_state && state){
+		ret = 1;
+	}
+	
+	last_state = state;
+	return ret;
+}
+
+void TampilkanPembacaanADC(){
+    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &adc_raw[0][0]));
         //ESP_LOGI("LOG", "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC_CHANNEL_0, adc_raw[0][0]);
         if (do_calibration1_chan0) {
             ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan0_handle, adc_raw[0][0], &voltage[0][0]));
             ESP_LOGI("LOG", "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, ADC_CHANNEL_0, voltage[0][0]);
         }
         vTaskDelay(pdMS_TO_TICKS(10));
-
         // ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_2, &adc_raw[0][1]));
         // ESP_LOGI("LOG", "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC_CHANNEL_2, adc_raw[0][1]);
         // if (do_calibration1_chan2) {
@@ -50,6 +76,33 @@ void app_main(void)
         //     ESP_LOGI("LOG", "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, ADC_CHANNEL_2, voltage[0][1]);
         // }
         // vTaskDelay(pdMS_TO_TICKS(1000));
+}
+
+void app_main(void)
+{
+    
+    ADC_Initialation();
+
+    GPIO_Initialation();
+
+    static int8_t ToggleState = 0;
+
+    while (1) {
+        
+        int button_state = gpio_get_level(Pin_Button);
+
+        if (button_pressed(button_state))
+        {
+            ToggleState = !ToggleState;
+            ESP_LOGI("Toggle Condition", "%d", ToggleState);
+        }
+
+        if(ToggleState)
+        {
+            TampilkanPembacaanADC();
+        }
+
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
 
@@ -65,7 +118,7 @@ static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel,
 
 #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
     if (!calibrated) {
-        ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
+        ESP_LOGI("Calibration", "calibration scheme version is %s", "Curve Fitting");
         adc_cali_curve_fitting_config_t cali_config = {
             .unit_id = unit,
             .chan = channel,
@@ -109,7 +162,7 @@ static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel,
 static void example_adc_calibration_deinit(adc_cali_handle_t handle)
 {
 #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
+    ESP_LOGI("Calibration", "deregister %s calibration scheme", "Curve Fitting");
     ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
 
 #elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
